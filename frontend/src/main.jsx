b1ee61093e9a0622e4bd40839d7f20b1a0f66ac1
sumit kumar <krsumit1002@gmail.com>
2025-11-25 20:05:29 +0530

    refactor: simplify main.jsx by removing StrictMode and using createRoot directly

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import Stairs from './components/Stairs.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-     <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
